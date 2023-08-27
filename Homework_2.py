a8d0574275c4faef1a2cfc731ad729bc390ad00d
Max Govorovsky <mgovorovsky@MacBook.local>
2023-08-27 13:53:37 +0300

    fourth commit added - checking hw_2

--- Homework_2.py
+++ Homework_2.py
@@ -4,8 +4,12 @@ print("Введите Ваш рост в см.:")
 b = int(input())
 index = a / (b/100 * b/100)
 weight_index = round(index, 1)
-position1 = round((weight_index - 20) / 1.66)   #18 items between 20 and 50
+x = 15
+y = 45
+steps = 18
+step = ((y - x) / steps)
+position1 = round((weight_index - x) / step)
 item1 = '='
-result1 = ('20',item1 * position1,'|',item1 * (18 - position1),'50')
+result1 = (str(x),item1 * position1,'|',item1 * (steps - position1),str(y))
 print("Ваш индекс массы тела:", weight_index)
 print("".join(result1))
