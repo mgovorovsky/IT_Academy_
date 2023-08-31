176e48e4883e4015b0804fd2a887c1ea9028f72e
Max Govorovsky <mgovorovsky@MacBook.local>
2023-08-31 23:44:16 +0300

    7th commit added

--- Homework_3.py
+++ Homework_3.py
@@ -4,10 +4,10 @@ print("Введите значение 2:")
 b = int(input())
 print("Введите значение 3:") 
 c = int(input())
-step_1 = a != 0 and b != 0 and c != 0 and "Нет нулевых значений!!!"
-print(step_1)
-step_2 = 0 or a or b or c or "Введены все нули!"
-print(step_2)
+step_1 = a and b and c and "Нет нулевых значений!!!"
+print('step1', step_1)
+step_2 = a or b or c or "Введены все нули!"
+print('step2', step_2)
 if a > (b + c):
     print(a - b - c)
 if a < (b + c):
