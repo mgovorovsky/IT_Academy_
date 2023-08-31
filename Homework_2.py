b34b4756c2796134e170568f1b4b1eb2ed59bbcb
Max Govorovsky <mgovorovsky@MacBook.local>
2023-09-01 00:12:05 +0300

    9th commit added

--- Homework_2.py
+++ Homework_2.py
@@ -10,6 +10,6 @@ steps = 18
 step = ((Y - X) / steps)
 position1 = round((weight_index - X) / step)
 item1 = '='
-result1 = (str(X),item1 * position1,'|',item1 * (steps - position1),str(Y))
+result1 = (str(X), item1 * position1, '|', item1 * (steps - position1), str(Y))
 print("Ваш индекс массы тела:", weight_index)
 print("".join(result1))
